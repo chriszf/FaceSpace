3337f3ba6caafe3860e11f8edb5745a4f31d1fa0
Christian Fernandez <christian_fernandez@intuit.com>
2015-11-03 10:04:22 -0800

    Adding placeholders for different training sets and algorithms

--- face.py
+++ face.py
@@ -19,7 +19,7 @@ def extract_face(img):
 
     faces = []
 
-    face_coords = FACE_CASCADE.detectMultiScale(img)
+    face_coords = FACE_CASCADE.detectMultiScale(img, minNeighbors=9)
 
     for x, y, w, h in face_coords:
         face = img[y:y+h, x:x+w]
@@ -49,22 +49,41 @@ def get_training_data(face_path="yalefaces/"):
     return faces
 
 
-def main(filename, reload=False):
+def get_recognizer(algorithm = "eigen", file_db = "snsd.eigen.yml", source_dir="yalefaces/", reload_ = False):
+    algos = {'eigen': cv2.createEigenFaceRecognizer,
+             'fisher': cv2.createFisherFaceRecognizer
+            }
+    recognizer = algos[algorithm]()
+    if not reload_ and os.path.exists(file_db):
+        recognizer.load(file_db)
+        return recognizer
 
-    recognizer = cv2.createEigenFaceRecognizer()
-    training_faces = get_training_data()
+    print "reloading"
+
+    training_faces = get_training_data(source_dir)
 
     face_imgs = [ face for face, _ in training_faces ]
     face_labels = np.array( [ id_ for _, id_ in training_faces ] )
 
     recognizer.train(face_imgs, face_labels)
+    recognizer.save(file_db)
+    return recognizer
+
+
+def main(filename, reload_=False):
+
+    recognizer = get_recognizer(reload_=reload_) # Default, full set, eigenfaces
+    #recognizer = get_recognizer(algorithm = "fisher", file_db = "snsd.fisher.yml", reload_=reload_) # Default, full set, eigenfaces
+    #recognizer = get_recognizer(algorithm = "fisher", file_db = "snsd_only.fisher.yml", source_dir="snsd_training/", reload_=reload_) # snsd set, fisher
+    #recognizer = get_recognizer(algorithm = "eigen", file_db = "snsd_only.eigen.yml", source_dir="snsd_training/", reload_=reload_) # snsd set, eigenfaces
+
 
     target_img = Image.open(filename).convert("L")
     img = np.array(target_img, "uint8")
     target_faces = extract_face(img)
     for f in target_faces:
         cv2.imshow("Face?", f)
-        cv2.waitKey(1000)
+        cv2.waitKey(2000)
         print recognizer.predict(f)
 
 
@@ -72,10 +91,11 @@ def parse_args(args):
     params = {}
     params['filename'] = args.pop(0)
 
+    #params['reload_'] = True
     while args:
         a = args.pop(0)
         if "a" == "-r":
-            params['reload'] = True
+            params['reload_'] = True
 
     return params
 
